0792dc5be863448dfbb294d7c9384b37f21fa97b
AnimeTheNeko <56701097+AnimeTheNeko@users.noreply.github.com>
2021-09-15 00:04:02 +0200

    icon and color changes

--- Interface.Designer.cs
+++ Interface.Designer.cs
@@ -29,10 +29,18 @@ namespace oleadmindb
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Interface";
             this.Text = "Interface";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
